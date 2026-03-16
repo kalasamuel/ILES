828b84f94ccf4972acf5288d4e65071b534efbf0
Kala Samuel <kalasamuel79@gmail.com>
2026-03-16 16:21:27 +0300

    serialisers

---
+++ dashboards/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import DashboardMetric
+
+
+class DashboardMetricSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DashboardMetric
+        fields = '__all__'
