31ba80dd54ed96c49aa0e891472dcf1ae3df56ca
Kala Samuel <kalasamuel79@gmail.com>
2026-03-16 16:26:49 +0300

    organisations serializers

---
+++ organizations/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Organization
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ['name', 'industry', 'city', 'country']
