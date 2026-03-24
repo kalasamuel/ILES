7784204cfc47422eed67a4763cbdfe3c09d5265b
Kala Samuel <kalasamuel79@gmail.com>
2026-03-24 16:24:11 +0300

    CORS settings

--- backend/iles/settings.py
+++ backend/iles/settings.py
@@ -149,9 +149,11 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:3001",
     "http://localhost:3002",
+    "http://localhost:5173",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:3001",
     "http://127.0.0.1:3002",
+    "http://127.0.0.1:5173",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
