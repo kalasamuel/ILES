0e01cd82e79edb4b9179eea347009b2895647086
Kala Samuel <kalasamuel79@gmail.com>
2026-04-04 20:58:20 +0300

    Removed extra spaces in metrics URL registration for DashboardMetricViewSet in urls.py

--- backend/dashboards/urls.py
+++ backend/dashboards/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import DashboardMetricViewSet
 
 router = DefaultRouter()
-router.register(r' metrics ', DashboardMetricViewSet)
+router.register(r'metrics', DashboardMetricViewSet)
 
 urlpatterns = [
     path('',  include(router.urls)),
