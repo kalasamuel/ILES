ea422e31a4de1fb72cf5bc79e1747c4b4c2c10af
AmanyireCI <amanyirecindy@gmail.com>
2026-04-09 23:02:23 +0300

    added comment

--- frontend/iles-frontend/src/services/apiClient.js
+++ frontend/iles-frontend/src/services/apiClient.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: "http://localhost:8000/api",
 })
 
-// Request interceptor to add auth token
+// Request interceptor to add the auth token
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('accessToken');
