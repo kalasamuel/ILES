1bd1bd0d4c8ddf929c9691a828b166d4935b3752
Kala Samuel <kalasamuel79@gmail.com>
2026-04-25 19:25:14 +0300

    refactored resetPassword API to use verification code instead of token

--- frontend/iles-frontend/src/services/endpoints.js
+++ frontend/iles-frontend/src/services/endpoints.js
@@ -14,9 +14,9 @@ export const authAPI = {
     const response = await api.post('/accounts/users/forgot-password/', { email });
     return response.data;
   },
-  resetPassword: async (token, new_password, confirm_password) => {
+  resetPassword: async (verificationCode, new_password, confirm_password) => {
     const response = await api.post('/accounts/users/reset-password/', {
-      token,
+      verification_code: verificationCode,
       new_password,
       confirm_password,
     });
