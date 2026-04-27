9a888fc511530035e2b54f49b915f66fc6fe7464
biar211 <elijahbiar2@gmail.com>
2026-04-27 17:41:15 +0300

    class evaluation criteria

--- backend/dashboards/serializers.py
+++ backend/dashboards/serializers.py
@@ -2,3 +2,8 @@ from rest_framework import serializers
 from django.utils import timezone
 from django.db import transaction
 from .models import EvaluationCriteria, Evaluation, EvaluationScore, ScoreBreakdown
+
+class EvaluationCriteriaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = EvaluationCriteria
+        fields = '__all__' 
