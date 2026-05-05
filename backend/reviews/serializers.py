2c9473945fae461c18cd7f42b471f68519ee388d
Kala Samuel <kalasamuel79@gmail.com>
2026-05-05 10:08:29 +0300

    Added supervisor field to LogReviewSerializer for write access

--- backend/reviews/serializers.py
+++ backend/reviews/serializers.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 from .models import LogReview, WorkflowHistory
 from accounts.serializers import UserSerializer
+from accounts.models import Supervisor
 
 
 class LogReviewSerializer(serializers.ModelSerializer):
     supervisor_details = UserSerializer(source='supervisor.user', read_only=True)
+    supervisor = serializers.PrimaryKeyRelatedField(queryset=Supervisor.objects.all(), required=False, write_only=True)
 
     class Meta:
         model = LogReview
